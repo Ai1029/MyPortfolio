7f9054cc2107a5eea0aadb9f00fce14ada0799e8
Ai1029 <ai.ishiwata1029.s@gmail.com>
2023-10-13 19:32:03 +0900

    CORSエラーの箇所修正

--- server/src/app.ts
+++ server/src/app.ts
@@ -31,7 +31,7 @@ app.use(
     credentials: true,
     origin: [
       "http://localhost:3000",
-      "https://my-portfolio-psi-swart-91.vercel.app/",
+      "https://my-portfolio-psi-swart-91.vercel.app",
     ], //アクセス許可するオリジン
     optionsSuccessStatus: 200, //レスポンスstatusを200に設定
   })
