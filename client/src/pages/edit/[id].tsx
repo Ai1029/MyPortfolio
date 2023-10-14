bf12d3d5296cfa907abdc4de89baf168c7164e37
Ai1029 <ai.ishiwata1029.s@gmail.com>
2023-10-14 19:40:25 +0900

    SSR調整

--- client/src/pages/edit/[id].tsx
+++ client/src/pages/edit/[id].tsx
@@ -10,7 +10,7 @@ import {
   createTheme,
   ThemeProvider,
 } from "@mui/material";
-import { getServerSideProps } from "../api/api";
+// import { getServerSideProps } from "../api/api";
 import { Props, SelectProps } from "../../../types/types";
 import UserInfoEdit from "../../components/edituser";
 import UserSkillEdit from "../../components/editskill";
@@ -21,8 +21,9 @@ import UserPortfolioEdit from "../../components/editeportfolio";
 import UserPortfolioCreate from "../../components/createportfolio";
 import UserSnsEdit from "../../components/editesns";
 import UserSnsCreate from "../../components/createsns";
-import { GetServerSideProps } from "next";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import axios from "axios";
+import { ParsedUrlQuery } from "querystring";
 
 const UserEdit: FC<Props & SelectProps> = ({
   userInfo,
@@ -87,5 +88,92 @@ const UserEdit: FC<Props & SelectProps> = ({
   );
 };
 
-export { getServerSideProps };
+// export { getServerSideProps };
 export default UserEdit;
+
+// ユーザー情報を取得する;
+export const getServerSideProps: GetServerSideProps = async ({
+  params,
+}: GetServerSidePropsContext<ParsedUrlQuery>) => {
+  const apiURL = process.env.API_URL_SSR;
+
+  try {
+    if (!params?.id) {
+      console.error("ユーザー情報がありません");
+      return {
+        props: {
+          userInfo: null,
+          userImage: null,
+          userSkill: null,
+          skillLevel: null,
+          userExperience: null,
+          experienceCategory: null,
+          year: null,
+          month: null,
+          userPortfolio: null,
+          userSns: null,
+          snsType: null,
+        },
+      };
+    }
+
+    const [
+      userRes,
+      userImageRes,
+      skillRes,
+      skilllevelRes,
+      experienceRes,
+      experienceCategoryRes,
+      yearRes,
+      monthRes,
+      portfolioRes,
+      snsRes,
+      snstypeRes,
+    ] = await Promise.all([
+      axios.get(`${apiURL}/api/v1/user/${params.id}`),
+      axios.get(`${apiURL}/api/v1/userimg/${params.id}`),
+      axios.get(`${apiURL}/api/v1/skill/${params.id}`),
+      axios.get(`${apiURL}/api/v1/skilllevel`),
+      axios.get(`${apiURL}/api/v1/experience/${params.id}`),
+      axios.get(`${apiURL}/api/v1/experiencecategory`),
+      axios.get(`${apiURL}/api/v1/year`),
+      axios.get(`${apiURL}/api/v1/month`),
+      axios.get(`${apiURL}/api/v1/portfolio/${params.id}`),
+      axios.get(`${apiURL}/api/v1/sns/${params.id}`),
+      axios.get(`${apiURL}/api/v1/typeofsns`),
+    ]);
+
+    return {
+      props: {
+        userInfo: userRes.data,
+        userImage: userImageRes.data,
+        userSkill: skillRes.data,
+        skillLevel: skilllevelRes.data,
+        userExperience: experienceRes.data,
+        experienceCategory: experienceCategoryRes.data,
+        year: yearRes.data,
+        month: monthRes.data,
+        userPortfolio: portfolioRes.data,
+        userSns: snsRes.data,
+        snsType: snstypeRes.data,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      props: {
+        userInfo: null,
+        userImage: null,
+        userSkill: null,
+        skillLevel: null,
+        userExperience: null,
+        experienceCategory: null,
+        year: null,
+        month: null,
+        userPortfolio: null,
+        userSns: null,
+        snsType: null,
+      },
+    };
+  }
+};
--- client/src/pages/userpage/[id].tsx
+++ client/src/pages/userpage/[id].tsx
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import React, { FC, useEffect, useState } from "react";
 import { signOut, onAuthStateChanged, User } from "firebase/auth";
 import { auth } from "../../../firebase/firebase";
-import { getServerSideProps } from "../api/api";
+// import { getServerSideProps } from "../api/api";
 import { Props } from "../../../types/types";
 import {
   Toolbar,
@@ -21,6 +21,9 @@ import UserpageSkill from "../../components/userpageskill";
 import UserpagePortfolio from "../../components/userpageportfolio";
 import UserpageExperience from "../../components/userpageexperience";
 import UserpageSns from "../../components/userpagesns";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import { ParsedUrlQuery } from "querystring";
+import axios from "axios";
 
 const Userpage: FC<Props> = ({
   userInfo,
@@ -110,5 +113,92 @@ const Userpage: FC<Props> = ({
   );
 };
 
-export { getServerSideProps };
+// export { getServerSideProps };
 export default Userpage;
+
+// ユーザー情報を取得する;
+export const getServerSideProps: GetServerSideProps = async ({
+  params,
+}: GetServerSidePropsContext<ParsedUrlQuery>) => {
+  const apiURL = process.env.API_URL_SSR;
+
+  try {
+    if (!params?.id) {
+      console.error("ユーザー情報がありません");
+      return {
+        props: {
+          userInfo: null,
+          userImage: null,
+          userSkill: null,
+          // skillLevel: null,
+          userExperience: null,
+          // experienceCategory: null,
+          // year: null,
+          // month: null,
+          userPortfolio: null,
+          userSns: null,
+          // snsType: null,
+        },
+      };
+    }
+
+    const [
+      userRes,
+      userImageRes,
+      // skillRes,
+      // skilllevelRes,
+      experienceRes,
+      // experienceCategoryRes,
+      // yearRes,
+      // monthRes,
+      portfolioRes,
+      snsRes,
+      // snstypeRes,
+    ] = await Promise.all([
+      axios.get(`${apiURL}/api/v1/user/${params.id}`),
+      axios.get(`${apiURL}/api/v1/userimg/${params.id}`),
+      axios.get(`${apiURL}/api/v1/skill/${params.id}`),
+      // axios.get(`${apiURL}/api/v1/skilllevel`),
+      axios.get(`${apiURL}/api/v1/experience/${params.id}`),
+      // axios.get(`${apiURL}/api/v1/experiencecategory`),
+      // axios.get(`${apiURL}/api/v1/year`),
+      // axios.get(`${apiURL}/api/v1/month`),
+      axios.get(`${apiURL}/api/v1/portfolio/${params.id}`),
+      axios.get(`${apiURL}/api/v1/sns/${params.id}`),
+      // axios.get(`${apiURL}/api/v1/typeofsns`),
+    ]);
+
+    return {
+      props: {
+        userInfo: userRes.data,
+        userImage: userImageRes.data,
+        // userSkill: skillRes.data,
+        // skillLevel: skilllevelRes.data,
+        userExperience: experienceRes.data,
+        // experienceCategory: experienceCategoryRes.data,
+        // year: yearRes.data,
+        // month: monthRes.data,
+        userPortfolio: portfolioRes.data,
+        userSns: snsRes.data,
+        // snsType: snstypeRes.data,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      props: {
+        userInfo: null,
+        userImage: null,
+        userSkill: null,
+        // skillLevel: null,
+        userExperience: null,
+        // experienceCategory: null,
+        // year: null,
+        // month: null,
+        userPortfolio: null,
+        userSns: null,
+        // snsType: null,
+      },
+    };
+  }
+};
